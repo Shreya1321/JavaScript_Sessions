3a88d950c12c3f462e3151423d6330552806dfcb
shreya1321 <shreyapatel9713@gmail.com>
2020-10-23 14:43:35 -0400

    First Session Committed

---
+++ DataTypes/null_demo.js
@@ -0,0 +1,23 @@
+/**
+ * null means absence of value.
+ * undefined means lack of value.
+ * A null and undefined value evaluates to false in condit/ional expression.
+ */
+
+/*
+    var myValue1 = null;
+    console.log(myValue1);
+    if (myValue1) {
+        console.log('Good')
+    } else {
+        console.log('Bad')
+    }
+*/
+
+var myValue2; // Undefined
+console.log(myValue2);
+if (myValue2) {
+    console.log('Good')
+} else {
+    console.log('Bad')
+}
