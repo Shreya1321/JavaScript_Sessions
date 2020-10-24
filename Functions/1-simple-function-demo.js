2564220f0cd4a49bff754bc957f835fadbce7605
shreya1321 <shreyapatel9713@gmail.com>
2020-10-23 23:36:56 -0400

    Async Programing

---
+++ Functions/1-simple-function-demo.js
@@ -0,0 +1,10 @@
+
+// Function Definition
+function sayHello() {
+    // console.log("Hello Everyone..!!")
+    alert('Hello Everyone..!!')
+}
+
+// sayHello();
+// sayHello();
+// sayHello();
---
+++ Objects/activity.js
@@ -0,0 +1,12 @@
+var number = 100;
+
+function MyFunc() {
+    this.number = 200;
+    console.log(number);
+    console.log(this.number)
+}
+
+// MyFunc();
+
+var obj = new MyFunc();
+console.log(obj.number);
